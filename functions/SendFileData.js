0ae6dc8089d4d17effe7ffd49245410d29d811c2
aqswdefr1234 <108164564+aqswdefr1234@users.noreply.github.com>
2024-03-14 11:24:12 +0900

    Update SendFileData.js

--- functions/SendFileData.js
+++ functions/SendFileData.js
@@ -1,8 +1,28 @@
 const fs = require('fs');
+function getSubDirectories(directory) {
+    // 현재 디렉토리의 하위 항목을 읽어옵니다.
+    const contents = fs.readdirSync(directory, { withFileTypes: true });
+    // 하위 디렉토리를 저장할 배열을 초기화합니다.
+    const subDirectories = [];
 
+    // 디렉토리 내의 모든 항목을 반복하여 하위 디렉토리를 찾습니다.
+    for (const item of contents) {
+        // 디렉토리인지 확인합니다.
+        if (item.isDirectory()) {
+            // 현재 항목이 디렉토리인 경우, 하위 디렉토리의 이름을 배열에 추가합니다.
+            subDirectories.push(item.name);
+        }
+    }
+
+    return subDirectories;
+}
 exports.handler = async function(event) {
     // 요청에서 파일 경로 가져오기
     console.log("요청중");
+    const currentDirectory = __dirname;
+    const subDirectories = getSubDirectories(currentDirectory);
+    console.log("Subdirectories in the current directory:", subDirectories);
+    
     const filePath = './ksjmetaverseshop/storagefile';
     // 파일 읽기
     try {
